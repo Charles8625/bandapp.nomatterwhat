d0296b6b32e96d5b71ae6c65157b65c3299abde9
Charles8625 <niflheim101@gmail.com>
2021-03-31 14:13:27 -0500

    Create User and Login

--- app/src/main/java/com/woythaltech/bandapp/nomatterwhat/BecomeVIP.java
+++ app/src/main/java/com/woythaltech/bandapp/nomatterwhat/BecomeVIP.java
@@ -1,19 +1,119 @@
 package com.woythaltech.bandapp.nomatterwhat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class BecomeVIP extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.jetbrains.annotations.NotNull;
+
+public class BecomeVIP extends AppCompatActivity implements View.OnClickListener {
+
+    private Button register;
+    private FirebaseAuth mAuth;
+    private TextView name;
+    private TextView email;
+    private TextView password;
+    private TextView confirmPW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_become_vip);
 
+        register = findViewById(R.id.register);
+        register.setOnClickListener(this);
 
+        name = findViewById(R.id.nameRegister);
+        email = findViewById(R.id.emailRegister);
+        password = findViewById(R.id.pwRegister);
+        confirmPW = findViewById(R.id.pwRegister2);
+
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.register){
+            registerUser();
+        }
+
+    }
+
+    private void registerUser() {
+        String email = this.email.getText().toString().trim();
+        String name = this.name.getText().toString().trim();
+        String password = this.password.getText().toString().trim();
+        String confirmPW = this.confirmPW.getText().toString().trim();
+
+        if (name.isEmpty()){
+            this.name.setError("You must enter a name");
+            this.name.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()){
+            this.email.setError("You must enter an email");
+            this.email.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()){
+            this.password.setError("You must enter a password");
+            this.password.requestFocus();
+            return;
+        }
+
+        if (confirmPW.isEmpty()){
+            this.confirmPW.setError("You must confirm your password");
+            this.confirmPW.requestFocus();
+            return;
+        }
+
+        if (password.length() < 8){
+            this.password.setError("Minimum password length is 8 characters");
+            this.password.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            this.email.setError("Please provide a valid email");
+            this.email.requestFocus();
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        User user = new User(name, email, password);
+                        FirebaseDatabase.getInstance().getReference("Users")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .setValue(user).addOnCompleteListener(task1 -> {
+                                    if(task1.isSuccessful()){
+                                        Toast.makeText(BecomeVIP.this,
+                                                "User has been registered successfully",
+                                                Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(BecomeVIP.this,
+                                                "Failed to Register", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        Toast.makeText(BecomeVIP.this, "Failed to Register the User",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
 }
--- app/src/main/java/com/woythaltech/bandapp/nomatterwhat/VIPFragment.java
+++ app/src/main/java/com/woythaltech/bandapp/nomatterwhat/VIPFragment.java
@@ -3,21 +3,33 @@ package com.woythaltech.bandapp.nomatterwhat;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
+import android.support.v4.app.INotificationSideChannel;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Objects;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class VIPFragment extends Fragment{
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Pattern;
+
+public class VIPFragment extends Fragment {
 
     private static final String TAG = "VIPFragment";
     private View view;
+    private FirebaseAuth mAuth;
+    private TextView vipEmail;
+    private TextView vipPassword;
 
 
     public VIPFragment() {
@@ -36,6 +48,10 @@ public class VIPFragment extends Fragment{
 
         view = inflater.inflate(R.layout.fragment_vip, container, false);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        vipEmail = view.findViewById(R.id.vipEmail);
+        vipPassword = view.findViewById(R.id.vipPassword);
 
         view.findViewById(R.id.vipLogin).setOnClickListener(v -> login());
         view.findViewById(R.id.becomeVIP).setOnClickListener(v -> becomeVIP());
@@ -45,7 +61,7 @@ public class VIPFragment extends Fragment{
     }
 
     private void login() {
-        Toast.makeText(this.getActivity(), "Logging In", Toast.LENGTH_SHORT).show();
+        userLogin();
     }
 
     private void becomeVIP() {
@@ -55,6 +71,38 @@ public class VIPFragment extends Fragment{
 
     private void forgotPW() {
         Toast.makeText(this.getActivity(), "Get new PW", Toast.LENGTH_SHORT).show();
+    }
+
+    private void userLogin() {
+
+        String email = vipEmail.getText().toString().trim();
+        String password = vipPassword.getText().toString().trim();
+
+        if(email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            vipEmail.setError("Please enter a valid email");
+            vipEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            vipPassword.setError("Password is Required");
+            vipPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 8){
+            vipPassword.setError("Password must be at least 8 Character");
+            vipPassword.requestFocus();
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                // Go to Profile Activity
+                startActivity(new Intent(getActivity(), Profile.class));
+            } else {
+                Toast.makeText(getActivity(), "Failed to Login. Please check your credentials", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
